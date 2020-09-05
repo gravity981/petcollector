e4dd5c4309b9d3ca6c289ede8071ca223b7e5654
Nethersoul <chrigi_ritz@hotmail.com>
2020-09-05 11:16:24 +0200

    - Added PID search for raspistill - Added signal call to raspistill

--- petcollector.py
+++ petcollector.py
@@ -6,7 +6,10 @@ from tinkerforge.bricklet_distance_ir import BrickletDistanceIR
 from tinkerforge.bricklet_dual_button import BrickletDualButton
 from tinkerforge.bricklet_dual_relay import BrickletDualRelay
 from resettabletimer import ResettableTimer
+from subprocess import check_output
+import os
 import sys
+import signal
 import pygame
 import time
 
@@ -23,6 +26,7 @@ ipcon = IPConnection()  # Create IP connection
 dir = BrickletDistanceIR(UID, ipcon)  # Create device object
 db = BrickletDualButton(UID_dual_button, ipcon)
 m_relay = BrickletDualRelay(UID_dual_relay, ipcon)  # Create device object
+m_raspistillPID = 0    #raspistill process id needed to send a camera trigger event
 pygame.mixer.init(44100, -16, 2, 1024)
 sound = pygame.mixer.Sound('laser.wav')
 
@@ -51,10 +55,16 @@ def dummy_callback(param):
         print("object in, count: " + str(objCount))
         isObjectPresent = True
         db.set_led_state(BrickletDualButton.LED_STATE_ON , BrickletDualButton.LED_STATE_ON )
+        os.kill(m_raspistillPID, signal.SIGUSR1)
 
+def getRaspistillPID():
+    global m_raspistillPID
+    m_raspistillPID = check_output(["pidof","raspistill"])
 
 def main():
     print("connect to tinkerforge deamon")
+    getRaspistillPID()
+    print("Raspistill PID:" + str(m_raspistillPID))
 
     sys.stdout.flush()
     ipcon.connect(HOST, PORT)  # Connect to brickd
