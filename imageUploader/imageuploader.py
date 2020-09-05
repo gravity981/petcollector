eeed300d648641ff5978d8facf04f026a2989ec8
Nicolas Marty <nicolas.marty@zuehlke.com>
2020-09-05 23:49:33 +0200

    start base64 encoding of image and http post only when file completely written

--- imageUploader/imageuploader.py
+++ imageUploader/imageuploader.py
@@ -4,6 +4,27 @@ from watchdog.events import FileSystemEventHandler
 import requests
 import base64
 import json
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from time import sleep
+
+executor = ThreadPoolExecutor(5)
+
+def say_after(delay, what):
+    sleep(delay)
+    what = what.rstrip('~')
+    print(what)
+    try:
+        with open(what, "rb") as image_file:
+            base64_encoded_image = base64.b64encode(image_file.read())
+            base64_message = base64_encoded_image.decode('utf-8')
+            data = dict()
+            data['machine_id'] = 'Olivia'
+            data['image'] = base64_message
+            r = requests.post('https://shrouded-inlet-73857.herokuapp.com/pint', json=data)
+            print("Response: HTTP " + str(r.status_code))
+    except FileNotFoundError:
+        print('file not found, skip')
 
 
 class Watcher:
@@ -36,22 +57,14 @@ class Handler(FileSystemEventHandler):
         elif event.event_type == 'created':
             # Take any action here when a file is first created.
             print("Received created event - %s." % event.src_path)
+            executor.submit(say_after, 0.3, event.src_path)
 
         elif event.event_type == 'modified':
             # Taken any action here when a file is modified.
             print("Received modified event - %s." % event.src_path)
-            try:
-                with open(event.src_path, "rb") as image_file:
-                    base64_encoded_image= base64.b64encode(image_file.read())
-                    base64_message = base64_encoded_image.decode('ascii')
-                    data = dict()
-                    data['machine_id'] = 'Olivia'
-                    data['image'] = base64_message
-                    #print(data)
-                    r = requests.post('https://shrouded-inlet-73857.herokuapp.com/pint', json=data)
-                    print("Response: HTTP " + str(r.status_code))
-            except FileNotFoundError:
-                print('file not found, skip')
+
+
+
 
 
 if __name__ == '__main__':
