cec005890547d931ebaf91798be10a3e26ee95d3
gravity981 <nicolas.marty@gmx.net>
2020-09-05 23:18:05 +0100

    crop images and save only region of interest

--- camera/cameraservice.sh
+++ camera/cameraservice.sh
@@ -3,5 +3,5 @@
 echo "start camera service"
 mkdir -p /home/pi/camera_data
 
-raspistill -t 0 -md 4 -w 1012 -h 760 -ex off --shutter 1000 -o /home/pi/camera_data/Image%d.jpg -s
+raspistill -t 0 -md 4 -w 1012 -h 760 -ex off --shutter 1000 -roi 0.15,0.4,0.6,0.45 -o /home/pi/camera_data/Image%d.jpg -s
 
