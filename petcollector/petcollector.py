90a9db0911c16a711b355eb87dc7699a2ac326c3
Nicolas Marty <nicolas.marty@zuehlke.com>
2020-09-05 22:36:01 +0200

    reorganize service subdirectories

---
+++ petcollector/petcollector.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_distance_ir import BrickletDistanceIR
+from tinkerforge.bricklet_dual_button import BrickletDualButton
+from tinkerforge.bricklet_dual_relay import BrickletDualRelay
+from tinkerforge.bricklet_oled_128x64 import BrickletOLED128x64
+from resettabletimer import ResettableTimer
+from subprocess import check_output
+import os
+import sys
+import signal
+import pygame
+import time
+import socketio
+
+HOST = "localhost"
+PORT = 4223
+UID = "xpN"  # UID of Distance IR Bricklet
+UID_dual_button = "mMX"
+UID_dual_relay = "E8x"
+UID_oled = "yqA"
+
+isObjectPresent = False
+objCount = 0
+
+ipcon = IPConnection()  # Create IP connection
+dir = BrickletDistanceIR(UID, ipcon)  # Create device object
+db = BrickletDualButton(UID_dual_button, ipcon)
+m_relay = BrickletDualRelay(UID_dual_relay, ipcon)  # Create device object
+display = BrickletOLED128x64(UID_oled, ipcon)
+pygame.mixer.init(44100, -16, 2, 1024)
+sound = pygame.mixer.Sound('laser.wav')
+backend_connected = False
+user_logged_in = False
+user_name = ""
+
+
+
+def timeout():
+    global isObjectPresent
+    timer.cancel()
+    if isObjectPresent:
+        print("object out")
+        isObjectPresent = False
+        db.set_led_state(not isObjectPresent, not backend_connected)
+
+
+timer = ResettableTimer(0.5, timeout)
+
+
+def updateDisplay():
+    global backend_connected
+    global user_logged_in
+    global user_name
+    display.clear_display()
+    display.write_line(0, 0, "PINT - Pet Collector")
+    if user_logged_in:
+        display.write_line(2, 0, "User " + str(user_name) + " logged in")
+    else:
+        display.write_line(2, 0, "Please Scan QR Code")
+    display.write_line(7, 0, "Object Count: " + str(objCount))
+
+
+def dummy_callback(param):
+    global isObjectPresent
+    global objCount
+    global backend_connected
+    timer.reset()
+    if not isObjectPresent:
+        sound.play()
+        timer.start()
+        objCount += 1
+        m_relay.set_monoflop(1, True, 200)
+        print("object in, count: " + str(objCount))
+        isObjectPresent = True
+        db.set_led_state(not isObjectPresent, not backend_connected)
+        os.system("pkill -USR1 raspistill")
+        display.write_line(7, 0, "Object Count: " + str(objCount))
+
+
+def main():
+    print("connect to tinkerforge deamon")
+
+    sys.stdout.flush()
+    ipcon.connect(HOST, PORT)  # Connect to brickd
+    # Don't use device before ipcon is connected
+    updateDisplay()
+
+    m_relay.set_state(False, False)
+    dir.set_debounce_period(400)
+
+    # distance in mm
+    dir.set_distance_callback_threshold(BrickletDistanceIR.THRESHOLD_OPTION_SMALLER, 150, 0)
+    dir.register_callback(BrickletDistanceIR.CALLBACK_DISTANCE_REACHED, dummy_callback)
+    sio = socketio.Client()
+
+    @sio.event
+    def connect():
+        global isObjectPresent
+        global backend_connected
+        print('connection to backend established')
+        backend_connected = True
+        updateDisplay()
+        db.set_led_state(not isObjectPresent, not backend_connected)
+
+    @sio.event
+    def disconnect():
+        global isObjectPresent
+        global backend_connected
+        print('disconnected from server')
+        backend_connected = False
+        updateDisplay()
+        db.set_led_state(not isObjectPresent, not backend_connected)
+
+    @sio.on('login_info')
+    def login_info(data):
+        global user_logged_in
+        global user_name
+        print('user logged in ', data)
+        user_logged_in = True
+        user_name = data['UserID']
+        print(str(db.get_led_state()))
+        updateDisplay()
+
+
+    @sio.on('logout_info')
+    def logout_info(data):
+        global user_logged_in
+        global user_name
+        print('user logged out ', data)
+        user_logged_in = False
+        user_name = ""
+        print(str(db.get_led_state()))
+        updateDisplay()
+
+    sio.connect('wss://shrouded-inlet-73857.herokuapp.com/')
+    # keep application running
+    try:
+        while True:
+            time.sleep(10)
+    except:
+        ipcon.disconnect()
+
+
+main()
--- petcollector/petcollector.sh
+++ petcollector/petcollector.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "start petcollector"
-cd /home/pi/petcollector
+cd /home/pi/petcollector/petcollector
 /usr/bin/python3 -u petcollector.py
 
 
