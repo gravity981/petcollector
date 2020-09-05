64c490ae3c14260a1ff187b9349348d52471d7a6
gravity981 <nicolas.marty@gmx.net>
2020-09-05 13:30:37 +0100

    Added index to images

--- camera/cameraservice.sh
+++ camera/cameraservice.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+!/bin/sh
 
 echo "start camera service"
 mkdir -p /home/pi/camera_data
-raspistill -t 1 -md 4 -w 1012 -h 760 -ex off --shutter 1000 -o /home/pi/camera_data/Image.jpg -s
+
+raspistill -t 0 -md 4 -w 1012 -h 760 -ex off --shutter 1000 -o /home/pi/camera_data/Image%04d.jpg -s
+
